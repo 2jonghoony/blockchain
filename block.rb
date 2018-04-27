4ace0de9c3e51d08131a350eaa336add276ded37
2jonghoony <dlwhdgns0402@gmail.com>
2018-04-27 21:25:50 +0900

    trace history

--- block.rb
+++ block.rb
@@ -4,10 +4,13 @@ class Blockchain
 
 	def mining
 
+	history = []
+
 	begin
 		nonce = rand(100000)
+		history << nonce
 		hashed = Digest::SHA256.hexdigest(nonce.to_s)
 	end while hashed[0..3] != "0000"
-	nonce
+	history
 	end
 end
