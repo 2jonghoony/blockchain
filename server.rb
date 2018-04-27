9bc810326b361d35e26c2b242df3130480094fa6
2jonghoony <dlwhdgns0402@gmail.com>
2018-04-27 22:41:33 +0900

    Display

--- server.rb
+++ server.rb
@@ -5,8 +5,18 @@ require './block'
 
 b = Blockchain.new
 
-get '/' do 
-	b.all_chains.to_s
+get '/' do
+	message = ""
+
+	b.all_chains.each do |a|
+		message << "번호는 : " + a["index"].to_s + "<br>"
+		message << "Nonce는 : " + a["nonce"].to_s + "<br>"
+		message << "시간은 : " + a["time"].to_s + "<br>"
+		message << "앞 주소는 : " + a["previous_address"].to_s + "<br>"
+		message << "내 주소는 : " + Digest::SHA256.hexdigest(a.to_s) + "<br>"
+		message << "<hr>"
+	end
+	message
 end
 
 get '/mine' do
