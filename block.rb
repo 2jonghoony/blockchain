08e5aca096657da958ebf3e214324b018fd33f57
2jonghoony <dlwhdgns0402@gmail.com>
2018-05-01 22:49:13 +0900

    do and view transaction

--- block.rb
+++ block.rb
@@ -7,6 +7,10 @@ class Blockchain
 		@wallet = {}
 	end
 
+	def wallet_list
+		@wallet
+	end
+
 	def make_a_wallet
 		address = SecureRandom.uuid.gsub("-", "") 
 		@wallet[address] = 100
@@ -14,13 +18,27 @@ class Blockchain
 	end
 
 	def make_a_trans(s, r, a)
-		trans = {
-			"sender" => s,
-			"rcpt" => r,
-			"amount" => a
-		}
-		@trans << trans
-		"다음 블럭에 쓰여집니다." + (@chain.length + 1).to_s
+
+		if @wallet[s].nil?
+			"보내는 지갑이 존재하지 않습니다."
+		elsif @wallet[r].nil?
+			"받는 지갑이 존재하지 않습니다."
+		elsif @wallet[s].to_f < a.to_f
+			"돈이 부족합니다."
+		else
+
+			@wallet[s] = @wallet[s] - a.to_f
+			@wallet[r] = @wallet[r] + a.to_f
+
+			trans = {
+				"sender" => s,
+				"rcpt" => r,
+				"amount" => a
+			}
+			@trans << trans
+			"다음 블럭에 쓰여집니다." + (@chain.length + 1).to_s
+		end
+
 	end
 
 	def mining
